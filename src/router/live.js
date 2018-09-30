ca892ff74552386dcf883b2ef39b4446f88f20e5
wuweixian <wwx@thetiger.com.cn>
2018-09-30 13:36:26 +0800

    ce

--- src/router/live.js
+++ src/router/live.js
@@ -6,19 +6,19 @@ const routers = [
     component: resolve => require(['@/views/live/index'], resolve)
   },
   {
-    path: '/live/room',
+    path: '/live_room',
     name: 'liveRoom',
     meta: { title: '直播间' },
     component: resolve => require(['@/views/live/liveRoom'], resolve)
   },
   {
-    path: '/live/detail',
+    path: '/live_detail',
     name: 'liveDetail',
     meta: { title: '直播详情' },
     component: resolve => require(['@/views/live/liveDetail'], resolve)
   },
   {
-    path: '/live/memberList',
+    path: '/live_memberList',
     name: 'memberList',
     meta: { title: '成员列表', pullDown: true },
     component: resolve => require(['@/views/live/memberList'], resolve)
