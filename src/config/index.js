c2aaa2be136230920d8aa8b6fa772b24989b4db3
wuweixian <wwx@thetiger.com.cn>
2018-10-19 21:12:21 +0800

    ce

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,10 @@ const settings = {
 // 全局设置
   // host: process.env.NODE_ENV !== 'production' ? '/lighthouse_wap' : '/beaconserver', // 接口根地址。线上使用的是Nginx代理
   host: process.env.NODE_ENV !== 'production' ? `/xplus_wap` : `http://wap-api.xplus.xiaodengta.com`, // 接口根地址。线上使用的是Nginx代理
-  dbPrefix: 'xplus_' // 本地存储的key
+  dbPrefix: 'xplus_', // 本地存储的key
+  productApi: 'http://wap-api.xplus.xiaodengta.com',
+  devApi: 'http://wap.xplus.ziwork.com',
+  devWsApi: 'ws://work-api.xplus.ziwork.com',
+  productWsApi: 'ws://work-api.xplus.xiaodengta.com'
 }
 export default settings
