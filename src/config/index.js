26145392f5ebc1954a6446878d57edfb82528dfc
wuweixian <wwx@thetiger.com.cn>
2018-10-22 21:15:04 +0800

    no message

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ const settings = {
   devApi: 'http://wap.xplus.ziwork.com', // 测试环境api
   devWsApi: 'ws://work-api.xplus.ziwork.com', // 测试环境webSocket
   productWsApi: 'ws://work-api.xplus.xiaodengta.com', // 生产环境webSocket
-  host: process.env.NODE_ENV !== 'production' ? `/xplus_wap` : process.type !== 'production' ? this.devApi : this.productApi,
-  websocketUrl: process.env.NODE_ENV !== 'production' ? this.devWsApi : process.type !== 'production' ? this.devWsApi : this.productWsApi
+  host: process.env.NODE_ENV !== 'production' ? `/xplus_wap` : process.type !== 'production' ? settings.devApi : settings.productApi,
+  websocketUrl: process.env.NODE_ENV !== 'production' ? settings.devWsApi : process.type !== 'production' ? settings.devWsApi : settings.productWsApi
 }
 export default settings
