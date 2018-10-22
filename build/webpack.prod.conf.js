a6d1471d3de9d54578efbc33c70683eb37cc3946
wuweixian <wwx@thetiger.com.cn>
2018-10-22 20:42:29 +0800

    no message

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -30,7 +30,8 @@ const webpackConfig = merge(baseWebpackConfig, {
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
-      'process.env': env
+      'process.env': env,
+      'productType': process.env.NODE_TYPE
     }),
     new UglifyJsPlugin({
       uglifyOptions: {
