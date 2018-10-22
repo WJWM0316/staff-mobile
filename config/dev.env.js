a19b5170468ac5feebca22523a54d49194036911
wuweixian <wwx@thetiger.com.cn>
2018-10-22 18:49:31 +0800

    no message

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: process.env.NODE_ENT
+  NODE_ENV: `${process.env.NODE_ENT}`
 })
