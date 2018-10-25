91d2989d78316f233fbe9ffc72a1d9458d573dab
wuweixian <wwx@thetiger.com.cn>
2018-10-25 19:05:16 +0800

    init

--- src/mixins/wechat.js
+++ src/mixins/wechat.js
@@ -175,7 +175,7 @@ export default {
         startRecord () {
           Vue.wx.startRecord({
             success: () => {
-              this.triggerCallback('onStartRecord')
+              this.triggerCallback('onStartRecord', res)
               Vue.wx.onVoiceRecordEnd({
                 // 录音时间超过一分钟没有停止的时候会执行 complete 回调
                 complete: res => {
