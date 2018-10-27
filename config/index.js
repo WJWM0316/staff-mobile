a204e3aa69bc597f352d7a9b24dd4ef72dc99e00
wuweixian <wwx@thetiger.com.cn>
2018-10-27 19:35:05 +0800

    cs

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/xplus_wap': {
-        target: `http://wap.xplus.ziwork.com/test`,
+        target: `//wap.xplus.ziwork.com/test`,
         changeOrigin: true,
         pathRewrite: {
           '^/xplus_wap': '/'
