34c4ff833842e266cd954fe99eb8f75a21861d2c
ChenHaoBin <531120903@qq.com>
2018-11-01 17:29:48 +0800

    测试微信上传接口

--- src/views/course/punchEditting.vue
+++ src/views/course/punchEditting.vue
@@ -100,8 +100,11 @@ export default {
     choseResult (res) {},
     /* 上传后返回 */
     upLoadResult (res) {
-      this.images.push(item.url)
-      this.uploadImgList.push(item.id)
+      alert(res)
+      res.forEach(item => {
+        this.images.push(item.url)
+        this.uploadImgList.push(item.id)
+      })
     },
     /* 选择图片 */
     photo () {
