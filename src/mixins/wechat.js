21c1b62560bd759d81824a73cd8c40aa3a8d3677
wuweixian <wwx@thetiger.com.cn>
2018-11-07 15:37:03 +0800

    no message

--- src/mixins/wechat.js
+++ src/mixins/wechat.js
@@ -34,7 +34,6 @@ export default {
      * 获取微信签名
      */
     async getWechatSign () {
-      if (this.$store.getters.wxConfig) return
       this.$store.dispatch('updata_wxConfig', this.wechatConfig)
       try {
         const params = {
