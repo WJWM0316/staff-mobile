001218e321cf672ce66c9b14c8608a8980207156
ChenHaoBin <531120903@qq.com>
2018-11-14 17:20:20 +0800

    合并

--- src/views/course/punchEditting.vue
+++ src/views/course/punchEditting.vue
@@ -188,7 +188,7 @@ export default {
             localstorage.remove('draft')
             localstorage.remove('draftImg')
             localstorage.remove('draftImgId')
-            this.$router.push({path: "/courseLesson",query: {id: this.$route.query.id}})
+            that.$router.push({path: '/courseLesson', query: {id: that.$route.query.id}})
           }
         })
       } catch (e) {
